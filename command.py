c660129bd1546ada51547265247de85da27efb48
Max Generowicz <git-mdcg@my-post-office.net>
2024-11-08 14:19:35 +0000

    Change way of initializing serial port

--- command.py
+++ command.py
@@ -1,11 +1,10 @@
 import serial
+import serial.tools.list_ports as ports
 import sys
 
 params = sys.argv
 command = params[1]
 
-with serial.Serial as ser:
-    port = "COM4"
-    baudrate = 115200
-    ser.open()
-    ser.write(command)
+ser = serial.Serial(port="COM4", baudrate=115200)
+ser.write(command)
+ser.close()
