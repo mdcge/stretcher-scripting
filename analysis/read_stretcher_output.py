08aa994909b4f9dfecf578ac180db29c4c53bad4
Max Generowicz <git-mdcg@my-post-office.net>
2024-11-20 11:06:33 +0000

    Correct data extraction script

--- analysis/read_stretcher_output.py
+++ analysis/read_stretcher_output.py
@@ -1,8 +1,25 @@
 import numpy as np
 
+# def read_stretcher_file(filename):
+#     with open(filename, 'r') as f:
+#         data = f.readlines()
+#     stripped_data = list(map(lambda x: x.strip(), data))
+#     extracted_values = list(map(lambda x: np.array([float(x[3:7]), float(x[10:14]), float(x[17:21])]), stripped_data))
+#     return np.array(extracted_values)[:,0], np.array(extracted_values)[:,1], np.array(extracted_values)[:,2]
+
 def read_stretcher_file(filename):
     with open(filename, 'r') as f:
         data = f.readlines()
     stripped_data = list(map(lambda x: x.strip(), data))
-    extracted_values = list(map(lambda x: np.array([float(x[3:7]), float(x[10:14]), float(x[17:21])]), stripped_data))
-    return np.array(extracted_values)[:,0], np.array(extracted_values)[:,1], np.array(extracted_values)[:,2]
+    values = []
+    for s in stripped_data:
+        v = ""
+        for char in s:
+            if char.isalpha():
+                values.append(v)
+                v = ""
+            else:
+                v += char
+    values = list(map(float, list(filter(None, values))))
+    packed_values = np.array([values[i:i+3] for i in range(0, len(values), 3)])
+    return packed_values[:,0], packed_values[:,1], packed_values[:,2]
