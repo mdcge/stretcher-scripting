539239c8eaee8c82158a259060579362f58220af
Max Generowicz <git-mdcg@my-post-office.net>
2024-12-03 11:28:18 +0000

    Add temperature file reader

--- analysis/read_stretcher_output.py
+++ analysis/read_stretcher_output.py
@@ -23,3 +23,10 @@ def read_stretcher_file(filename):
     values = list(map(float, list(filter(None, values))))
     packed_values = np.array([values[i:i+3] for i in range(0, len(values), 3)])
     return packed_values[:,0], packed_values[:,1], packed_values[:,2]
+
+def read_temperature_file(filename):
+    with open(filename, 'r', encoding="windows-1252") as f:
+        data = f.readlines()
+    just_data = list(map(lambda x: x.strip().split('\t'), data[29:]))
+    clean_data = np.array(list(filter(lambda x: len(x) == 4, just_data)))
+    return np.int32(clean_data[:,0]), clean_data[:,1], np.float64(clean_data[:,2]), np.float64(clean_data[:,3])
